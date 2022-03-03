5e4e83963005c0f0f441ecb42863a3160fa841ab
Bohdan Hawrylyshyn <d3cembry@gmail.com>
2022-03-04 00:11:46 +0300

    init

---
+++ CryptoTestTask/RootViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  CryptoTestTask
+//
+//  Created by Bohdan Hawrylyshyn on 04.03.2022.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+    private var current: UIViewController
+    
+    // MARK: - Init
+    
+    init() {
+        self.current = CryptoViewController()
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - ViewController lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addChild(current)
+        current.view.frame = view.bounds
+        view.addSubview(current.view)
+        current.didMove(toParent: self)
+    }
+    
+}
+
+// MARK: - Extensions
+
+extension AppDelegate {
+    static var shared: AppDelegate {
+        return UIApplication.shared.delegate as! AppDelegate
+    }
+    var rootViewController: RootViewController {
+        return window!.rootViewController as! RootViewController
+    }
+}
